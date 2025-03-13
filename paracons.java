9327d2092879d31c62370e3b7d486a3de745099e
kamalesh-jeyakumar <kamaleshj1211@gmail.com>
2025-03-13 16:39:58 +0530

    push

---
+++ paracons.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class paracons
+{
+  	 int a;
+   
+	 paracons(int a )
+	{
+ 	this.a =a ;
+	}
+public static void main (String args[])
+
+	{
+   	Scanner scan = new Scanner(System.in);
+  	 System.out.println("enter a number");
+ 	int a = scan.nextInt(); 
+ 	 paracons odd = new paracons(a);
+
+  
+ 	if( a % 2 ==0 )
+	{
+ 	 System.out.println(" It is a even number");
+	}
+	else
+	{
+ 	System.out.println(" It is odd number");
+	}
+	} 
+}
+ 
+  
